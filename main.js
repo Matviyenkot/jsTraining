2b0402b5c8821e039ef2a07cce8d45aa5db86924
Taras <matviyenkot@gmail.com>
2021-08-16 00:51:13 +0300

    learn functions

--- main.js
+++ main.js
@@ -18,7 +18,23 @@
 
 // alert();
 
-const numOfFilms = +prompt('how much movies have you watched?', '2');
+let numOfFilms;
+
+function start(){
+     numOfFilms = +prompt('how much movies have you watched?', '2');
+
+     while (numOfFilms == '' || numOfFilms == null || isNaN(numOfFilms) || numOfFilms < 0) {
+        numOfFilms = +prompt('how much movies have you watched?', '2');
+     }
+}
+
+function writeYourGenres (){
+    for (let i = 1; i<=3; i++){
+        const g = prompt(`your favorite genre on ${i} position`, '');
+        PersonalMovieDB.genres[i-1] = g;
+        console.log("done");
+    }
+}
 
 
 const PersonalMovieDB = {
@@ -29,35 +45,62 @@ const PersonalMovieDB = {
     private: false
 };
 
-    
-
-// for (let i=0;i<2;i++) {
-//     const a = prompt('what is last movie?', ''),
-//           b = +prompt('which ball you give?', '');
-
-//     if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
-//     PersonalMovieDB.movies[a] = b;
-//     console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
-
-let num = 0;
-
-while(num <2){
-    const a = prompt('what is last movie?', ''),
-          b = +prompt('which ball you give?', '');
-          num++;
-          if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+    function rememberMyFilms(){
+        for (let i=0;i<2;i++) {
+            const a = prompt('what is last movie?', ''),
+                  b = +prompt('which ball you give?', '');
+        
+            if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
             PersonalMovieDB.movies[a] = b;
             console.log('done');
             } else {
                 console.log('error');
-                num--;
+                i--;
             }
-}
+        }
+    }
+
+    function detectPersonalLevel(){
+        if (PersonalMovieDB.count <= 10){
+            console.log('few');
+        } else if (PersonalMovieDB.count > 10 && PersonalMovieDB < 30) {
+            console.log('normal');
+        } else if (PersonalMovieDB.count >= 30 ){
+            console.log('a lot');
+        } else {
+            console.log('not found');
+        }
+    }
+
+    function showMyDB(){
+        if (PersonalMovieDB.private == false){
+            console.log(PersonalMovieDB);
+        }
+    }
+
+    start();
+
+    rememberMyFilms();
+
+    writeYourGenres();
+    
+    detectPersonalLevel();
+
+    showMyDB();
+// let num = 0;
+
+// while(num <2){
+//     const a = prompt('what is last movie?', ''),
+//           b = +prompt('which ball you give?', '');
+//           num++;
+//           if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             PersonalMovieDB.movies[a] = b;
+//             console.log('done');
+//             } else {
+//                 console.log('error');
+//                 num--;
+//             }
+// }
 
 // do{
 //     const a = prompt('what is last movie?', ''),
@@ -73,15 +116,6 @@ while(num <2){
 // }
 // while(num<2);
 
-if (PersonalMovieDB.count <= 10){
-    console.log('few');
-} else if (PersonalMovieDB.count > 10 && PersonalMovieDB < 30) {
-    console.log('normal');
-} else if (PersonalMovieDB.count >= 30 ){
-    console.log('a lot');
-} else {
-    console.log('not found');
-}
 
 
-console.log(PersonalMovieDB);
+
