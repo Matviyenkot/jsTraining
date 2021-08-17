c740d4a7872415df6eb10eeec2cfaaea905a7912
Taras <matviyenkot@gmail.com>
2021-08-17 19:52:11 +0300

    oop begin

--- main.js
+++ main.js
@@ -1,66 +1,35 @@
 "use strict";
 
-// const result = confirm("are you here?");
-// console.log(result);
-
-// const answers = [];
-
-// answers[0] = prompt('what is your name?', '');
-// answers[1] = prompt('what is your surname?', '');
-// answers[2] = prompt('how old are you?', '');
-
-// document.write( typeof(answers) );
-
-// const myname = "Taras";
-
-// console.log (`Hi, ${myname}`);
-
-
-// alert();
-
-let numOfFilms;
-
-function start(){
-     numOfFilms = +prompt('how much movies have you watched?', '2');
-
-     while (numOfFilms == '' || numOfFilms == null || isNaN(numOfFilms) || numOfFilms < 0) {
-        numOfFilms = +prompt('how much movies have you watched?', '2');
-     }
-}
-
-function writeYourGenres (){
-    for (let i = 1; i<=3; i++){
-        const g = prompt(`your favorite genre on ${i} position`, '');
-        PersonalMovieDB.genres[i-1] = g;
-        console.log("done");
-    }
-}
-
-
 const PersonalMovieDB = {
-    count: numOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
-};
+    private: false,
+    start: function(){
+         PersonalMovieDB.count = +prompt('how much movies have you watched?', '2');
+   
+        while (PersonalMovieDB.count == '' || PersonalMovieDB.count == null || isNaN(PersonalMovieDB.count) || PersonalMovieDB.count < 0) {
+            PersonalMovieDB.count = +prompt('how much movies have you watched?', '2');            
+        }
+   },
 
-    function rememberMyFilms(){
-        for (let i=0;i<2;i++) {
-            const a = prompt('what is last movie?', ''),
-                  b = +prompt('which ball you give?', '');
-        
-            if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
-            PersonalMovieDB.movies[a] = b;
-            console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
+   rememberMyFilms: function(){
+    for (let i=0;i<2;i++) {
+        const a = prompt('what is last movie?', ''),
+              b = +prompt('which ball you give?', '');
+    
+        if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+        PersonalMovieDB.movies[a] = b;
+        console.log('done');
+        } else {
+            console.log('error');
+            i--;
         }
     }
+    },
 
-    function detectPersonalLevel(){
+    detectPersonalLevel: function(){
         if (PersonalMovieDB.count <= 10){
             console.log('few');
         } else if (PersonalMovieDB.count > 10 && PersonalMovieDB < 30) {
@@ -70,51 +39,43 @@ const PersonalMovieDB = {
         } else {
             console.log('not found');
         }
-    }
+    },
 
-    function showMyDB(){
+    showMyDB: function(){
         if (PersonalMovieDB.private == false){
             console.log(PersonalMovieDB);
         }
+    },
+
+    toggleVisibleMyDB:function(){
+        if (PersonalMovieDB.private){
+            PersonalMovieDB.private = false;
+        } else {
+            PersonalMovieDB.private = true;
+        }
+    },
+
+    writeYourGenres: function (){
+        for (let i = 1; i<=3; i++){
+            const g = prompt(`your favorite genre on ${i} position`, '');
+            if ( g != null  || g != '') {
+                PersonalMovieDB.genres[i-1] = g;
+            console.log("done");
+                } else {
+                    console.log('error');
+                    i--;
+                }
+        }
+    PersonalMovieDB.genres.forEach((item,i) => {
+        console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    } ) ;   
     }
+};
 
-    start();
+PersonalMovieDB.writeYourGenres();
 
-    rememberMyFilms();
-
-    writeYourGenres();
     
-    detectPersonalLevel();
 
-    showMyDB();
-// let num = 0;
-
-// while(num <2){
-//     const a = prompt('what is last movie?', ''),
-//           b = +prompt('which ball you give?', '');
-//           num++;
-//           if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             PersonalMovieDB.movies[a] = b;
-//             console.log('done');
-//             } else {
-//                 console.log('error');
-//                 num--;
-//             }
-// }
-
-// do{
-//     const a = prompt('what is last movie?', ''),
-//           b = +prompt('which ball you give?', '');
-//           num++;
-//           if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             PersonalMovieDB.movies[a] = b;
-//             console.log('done');
-//             } else {
-//                 console.log('error');
-//                 num--;
-//             }
-// }
-// while(num<2);
 
 
 
